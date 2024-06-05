1ba073f36bdc1e28f592aa8b854f00bcd2b58942
Toan <hahatyw@gmail.com>
2024-06-05 11:29:41 +0700

    Initial commit

---
+++ student/index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type ="text/css" href="style.css">
+    <title>Online Library Management</title>
+    
+</head>
+<?php
+    
+    include "navbar.php";
+?>
+<body>
+    
+        <div class="index-section"  style="background-image: url(./images/index-img.jpg); margin-top: -20px;height:650px; width: inherit;">
+            <br><br><br>
+            <div class="admin_section_img">
+                <div class="box" style="height: 300px;width: 450px;background-color: grey;margin:  70px auto;opacity: .7;color: white;">
+                    <br><br><br><br>
+                        <h1 style="text-align: center;font-size:30px">Welcome To Library</h1><br><br>
+                        <h1 style="text-align: center;font-size:20px ;">Opens at 09:00</h1><br>
+                        <h1 style="text-align: center;font-size:20px ;">Close at 15:00</h1>
+                </div>
+            </div>
+            </div>
+        <footer class="index-footer" style="height: 82px;width: inherit;background-color: black;color: white;text-align: center;align-items: center;padding-top: 30px;">
+            <p>
+                Email: library@gmail.com
+                <br>
+                Hotline: 19001234
+            </p>
+            
+            
+        </footer>
+</body>
+</html>
